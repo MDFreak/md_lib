d79664cc7edf7064f103635708bd078811f8fe34
MDFreak2 <75547040+MDFreak2@users.noreply.github.com>
2021-03-15 02:15:55 +0100

    --- Auto-Git Commit ---

--- src/test_md_lib.h
+++ src/test_md_lib.h
@@ -15,43 +15,46 @@
       #define TEST_MD_OLED
       //#define TEST_MD_FRAM
       //#define TEST_MD_WIFI
-    #endif
 
-  #if defined( TEST_MD_LCD )
-      void setup_lcd();
-      void lcd_loop();
-    #endif
-  #if defined( TEST_MD_BUZZER)
-      void setup_buzzer();
-      void buzzer_loop();
-    #endif
-  #if defined( TEST_MD_KEYPADSHIELD)
-      void setup_keypad();
-      void keypad_loop();
-    #endif
-  #if defined( TEST_MD_TOUCH)
-      void setup_touch();
-      void touch_loop();
-    #endif
-  #if defined( TEST_MD_WEBSERVER)
-      void setup_webserver();
-      void webserver_loop();
-    #endif
-  #if defined( TEST_MD_LED)
-      void setup_led();
-      void led_loop();
-    #endif
-  #if defined( TEST_MD_OLED)
-      void setup_oled();
-      void oled_loop();
-    #endif
-  #if defined( TEST_MD_FRAM)
-      void setup_fram();
-      void fram_loop();
-    #endif
-  #if defined( TEST_MD_WIFI)
-      void setup_wifi();
-      void wifi_loop();
+      #include <md_util.h>
+      #include <md_defines.h>
+
+      #if defined( TEST_MD_LCD )
+          void setup_lcd();
+          void lcd_loop();
+        #endif
+      #if defined( TEST_MD_BUZZER)
+          void setup_buzzer();
+          void buzzer_loop();
+        #endif
+      #if defined( TEST_MD_KEYPADSHIELD)
+          void setup_keypad();
+          void keypad_loop();
+        #endif
+      #if defined( TEST_MD_TOUCH)
+          void setup_touch();
+          void touch_loop();
+        #endif
+      #if defined( TEST_MD_WEBSERVER)
+          void setup_webserver();
+          void webserver_loop();
+        #endif
+      #if defined( TEST_MD_LED)
+          void setup_led();
+          void led_loop();
+        #endif
+      #if defined( TEST_MD_OLED)
+          void setup_oled();
+          void oled_loop();
+        #endif
+      #if defined( TEST_MD_FRAM)
+          void setup_fram();
+          void fram_loop();
+        #endif
+      #if defined( TEST_MD_WIFI)
+          void setup_wifi();
+          void wifi_loop();
+        #endif
     #endif
 
 #endif
