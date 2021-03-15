4977691954acb5dbdb38622cb8aecc71f9690385
MDFreak2 <75547040+MDFreak2@users.noreply.github.com>
2021-03-16 00:33:36 +0100

    --- Auto-Git Commit ---

--- src/test_md_oled.cpp
+++ src/test_md_oled.cpp
@@ -3,7 +3,29 @@
 #include "test_md_lib.h"
 
 #if defined(TEST_MD_LIB_ACTIVE)
-  #if defined( TEST_MD_OLED)
+  #if defined(TEST_MD_OLED)
+
+    #define OLED1
+    //#define OLED2
+
+    #if defined(OLED1)
+        #define I2C_ADDR_OLED1
+        #define I2C_SDA_OLED1
+        #define I2C_SCL_OLED1
+        #define OLEDDISPLAY_GEOMETRY
+        #define OLED1_GEO
+
+        md_oled oled1 = md_oled((uint8_t) I2C_ADDR_OLED1, (uint8_t) I2C_SDA_OLED1,
+                                (uint8_t) I2C_SCL_OLED1, (OLEDDISPLAY_GEOMETRY) OLED1_GEO);
+      #endif
+
+    #if defined(OLED2)
+        md_oled oled2 = md_oled((uint8_t) I2C_ADDR_OLED2, (uint8_t) I2C_SDA_OLED2,
+                                (uint8_t) I2C_SCL_OLED2, (OLEDDISPLAY_GEOMETRY) OLED2_GEO);
+      #endif
+
+    msTimer oledT   = msTimer(DISP_CYCLE);
+    uint8_t oledIdx = 0;
 
     void setup_oled()
       {
